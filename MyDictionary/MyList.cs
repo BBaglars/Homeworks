e63a3a5184b6a423dd4c9feb52fe480563989473
Berkay Bağlars <baglarsberkay38@gmail.com>
2023-07-13 18:01:16 +0300

    13.07.2023

---
+++ MyDictionary/MyList.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyDictionary
+{
+    internal class MyList<T>
+    {
+        T[] _array;
+        public MyList()
+        {
+            _array = new T[0];
+        }
+
+        public void Add(T item)
+        {
+            T[] _temparray = _array;
+            _array = new T[_array.Length + 1];
+            for (int i = 0; i < _temparray.Length; i++)
+            {
+                _array[i] = _temparray[i];
+            }
+            _array[_array.Length - 1] = item;
+        }
+    }
+   
+
+}
---
+++ MyDictionary/Program.cs
@@ -0,0 +1,6 @@
+﻿using MyDictionary;
+
+List<string> listem = new List<string>();
+
+listem.Add("Arda");
+Console.WriteLine(listem.Count);
